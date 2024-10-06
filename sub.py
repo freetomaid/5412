86e6083b356e63ee3aeeb36a0331c4f76b644063
Free to Maid <153776417+freetomaid@users.noreply.github.com>
2024-10-06 22:19:56 +0700

    Update sub.py

--- sub.py
+++ sub.py
@@ -4,7 +4,7 @@ from datetime import date
 import pytz
 
 BOT_TOKEN = "7260168327:AAEEOvNziECXLf29xfIU9rsr9JqIcmOFR_g"
-CHAT_ID = -1002345920504
+CHAT_ID = -1002009353137
 current_time = datetime.datetime.now(pytz.timezone('Asia/Jakarta'))
 today = today = date.today()
 
