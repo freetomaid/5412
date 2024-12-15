4e2d5be9aaa229aa0359984a4873faafb3302be0
Free to Maid <153776417+freetomaid@users.noreply.github.com>
2024-12-15 19:25:34 +0700

    Update respon.py

--- respon.py
+++ respon.py
@@ -27,7 +27,7 @@ def main():
                 document=fin,
                 # Up to 1024 characters.
                 # https://core.telegram.org/bots/api#inputmediadocument
-                caption=f"Total: {len(fin.readlines())} akun\n\nDiperbarui pada: {current_time}"
+                caption=f"Diperbarui pada: {current_time}"
             )
 
 
